0229168d049036404a83c5373c71ae2cb66c425b
rahils1 <rahil15973@gmail.com>
2024-10-27 17:56:52 -0700

    Update PageHandler.java

--- PageHandler.java
+++ PageHandler.java
@@ -1,16 +1,21 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class PageHandler extends Application {
-    LoginPage lp;
-    
-    public static void main(String[] args) {launch(args);}
+    private static Stage primaryStage;
+    private LoginPage lp;
+
+    public static void main(String[] args) { launch(args); }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        primaryStage = stage;
         lp = new LoginPage();
         primaryStage.setTitle(lp.getString());
         primaryStage.setScene(lp.getScene());
         primaryStage.show();
     }
+
+    public static void updatePage(Scene sc) {primaryStage.setScene(sc);}
 }
